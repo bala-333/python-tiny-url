e7f34b37b7c2e445f54c134966fd81e962ec69ea
venkatesh karnati <bala-333@bala3339@gmai.com>
2021-11-08 21:35:43 +0530

    Docker file and test casees added

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app import app as flask_app
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_tiny_url(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
